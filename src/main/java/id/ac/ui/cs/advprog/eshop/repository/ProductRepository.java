2a64b55224e610d69e2860c3c4eb5308caf6a117
noQils <daffaaqilmahmud@gmail.com>
2025-02-13 18:33:10 +0700

    ProductRepository.java (added findById and edit function)

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,4 +19,24 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product edit(String productId, Product updatedProduct) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                product.setProductName(updatedProduct.getProductName());
+                product.setProductQuantity(updatedProduct.getProductQuantity());
+                return product;
+            }
+        }
+        return null;
+    }
 }
