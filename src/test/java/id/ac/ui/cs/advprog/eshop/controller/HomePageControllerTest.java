7643991d85b874369b559cfcbe83d6d167fb3349
noQils <daffaaqilmahmud@gmail.com>
2025-02-19 23:45:45 +0700

    test controller (HomePageControllerTest.java, added comment in setUp function)

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -14,6 +14,7 @@ class HomePageControllerTest {
     HomePageController homePageController;
     @BeforeEach
     void setUp() {
+        // This method is kept for maintainability, could be useful for future needs
     }
 
     @Test
