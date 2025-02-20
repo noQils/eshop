7263ec7e639e0698aa7658aeb48193a4fd1b20df
noQils <daffaaqilmahmud@gmail.com>
2025-02-20 17:50:24 +0700

    fixed HomePageController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
     @GetMapping("/")
     public String home() {
-        return "homepage";
+        return "HomePage";
     }
 }
