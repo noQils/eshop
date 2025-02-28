33c5e1dbac9959a720959e9c0f1ff4c6f3999ba3
noQils <daffaaqilmahmud@gmail.com>
2025-02-28 17:09:05 +0700

    issue fixed (removed unused import)

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Iterator;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
--- src/test/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImplTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
