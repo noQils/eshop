63712dc2c0d6036b18110593643665e0b52a516f
noQils <daffaaqilmahmud@gmail.com>
2025-03-07 13:54:25 +0700

    [GREEN] Implement sub-feature VoucherCode validation in Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -16,24 +16,7 @@ public class Payment {
 
     public Payment(String id, String method, Map<String, String> paymentData) {
         this.id = id;
-        this.status = PaymentStatus.PENDING.getValue();
-
-        if (PaymentMethod.contains(method)) {
-            this.method = method;
-        } else {
-            throw new IllegalArgumentException();
-        }
-
-        if (paymentData.isEmpty()) {
-            throw new IllegalArgumentException();
-        } else {
-            this.paymentData = paymentData;
-        }
-    }
-
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {
-        this(id, method, paymentData);
-        this.setStatus(status);
+        this.dataValidation(method, paymentData);
     }
 
     public void setStatus(String status) {
@@ -42,5 +25,56 @@ public class Payment {
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void setMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void dataValidation(String method, Map<String, String> paymentData) {
+        this.setMethod(method);
+
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.paymentData = paymentData;
+        }
+
+        boolean validationStatus = false;
+        if (method.equals("VoucherCode")) {
+            validationStatus = validateVoucherCode(paymentData);
+        }
+        if (validationStatus) {
+            this.setStatus("SUCCESS");
+        } else {
+            this.setStatus("REJECTED");
+        }
+    }
+
+    public boolean validateVoucherCode(Map<String, String> paymentData) {
+        if (!paymentData.containsKey("voucherCode")) {
+            return false;
+        }
+        String voucherCode = paymentData.get("voucherCode");
+        if (voucherCode == null) {
+            return false;
+        } else if (voucherCode.length() != 16) {
+            return false;
+        } else if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+
+        int numericCount = 0;
+        for (int i = 0; i < voucherCode.length(); i++) {
+            if (Character.isDigit(voucherCode.charAt(i))) {
+                numericCount++;
+            }
+        }
+
+        return numericCount == 8;
     }
 }
