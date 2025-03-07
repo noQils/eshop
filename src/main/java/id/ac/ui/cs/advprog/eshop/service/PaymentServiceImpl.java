478722afeceadb9203110b977001ae97e2dbea55
noQils <daffaaqilmahmud@gmail.com>
2025-03-07 12:38:52 +0700

    [GREEN] Implement PaymentServiceImpl class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
@@ -10,30 +9,54 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final OrderService orderService;
 
     @Autowired
-    private PaymentRepository paymentRepository;
+    private PaymentServiceImpl(PaymentRepository paymentRepository, OrderService orderService) {
+        this.paymentRepository = paymentRepository;
+        this.orderService = orderService;
+    };
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        String paymentId = UUID.randomUUID().toString();
+        Payment payment = new Payment(paymentId, method, paymentData);
+
+        return paymentRepository.save(payment, order);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        if (payment == null) {
+            throw new NoSuchElementException();
+        }
+
+        Order order = paymentRepository.findOrderByPayment(payment.getId());
+        if (status.equals("SUCCESS")) {
+            payment.setStatus("SUCCESS");
+            orderService.updateStatus(order.getId(), "SUCCESS");
+            return payment;
+        } else if (status.equals("REJECTED")) {
+            payment.setStatus("REJECTED");
+            orderService.updateStatus(order.getId(), "FAILED");
+            return payment;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAllPayments();
     }
 }
